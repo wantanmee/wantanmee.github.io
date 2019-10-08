dd872c44748f44af8964405fd2e97a964c90cec4
Chen, Kate <kate.chen@moodys.com>
2019-10-08 17:20:19 +0800

    init commit

---
+++ _site/assets/js/search-data.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "研究了下ffmpeg",
+        "excerpt":"用ffmpeg来压手机拍的视频，首先可以参考下这两篇FFmpeg and H.265 Encoding Guide ","categories": ["blog"],
+        "tags": ["ffmepg"],
+        "url": "http://localhost:4000/blog/2019/10/08/use-ffmpeg.html"
+      }]
